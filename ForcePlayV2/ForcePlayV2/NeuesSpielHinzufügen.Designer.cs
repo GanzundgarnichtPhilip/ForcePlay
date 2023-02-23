ee9e51aa6b92057d47b4d76a4203dd59f215e7d2
Raphael Tack <raphael.t7@gso.schule.koeln>
2023-02-23 22:43:01 +0100

    Änderung am Design für Neue Spiele Hinzufügen:
    Folgende Änderungen wurden vorgenommen:
    	- Hinzufügen von drei weiteren Textboxen.
    	- Hinzufügen einer ComboBox.
    	- Placeholder wurden implementiert.
    	- Folgende Informationen sollen eingetragen werden:
    		- Titel
    		- Installationsdatum
    		- Zuletzt gespielt
    		- Installationspfad
    		- Kategorie
    		- Publisher
    		- USK

--- ForcePlayV2/ForcePlayV2/NeuesSpielHinzufügen.Designer.cs
+++ ForcePlayV2/ForcePlayV2/NeuesSpielHinzufügen.Designer.cs
@@ -29,61 +29,102 @@
         private void InitializeComponent()
         {
             this.SpielHinzufügenButton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.titel = new System.Windows.Forms.TextBox();
+            this.Installationsdatum = new System.Windows.Forms.TextBox();
+            this.zuletztGespielt = new System.Windows.Forms.TextBox();
+            this.installationspfad = new System.Windows.Forms.TextBox();
+            this.kategorie = new System.Windows.Forms.TextBox();
+            this.publisher = new System.Windows.Forms.TextBox();
+            this.usk = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // SpielHinzufügenButton
             // 
-            this.SpielHinzufügenButton.Location = new System.Drawing.Point(12, 175);
+            this.SpielHinzufügenButton.Location = new System.Drawing.Point(12, 307);
             this.SpielHinzufügenButton.Name = "SpielHinzufügenButton";
             this.SpielHinzufügenButton.Size = new System.Drawing.Size(210, 97);
             this.SpielHinzufügenButton.TabIndex = 0;
             this.SpielHinzufügenButton.Text = "Submit";
             this.SpielHinzufügenButton.UseVisualStyleBackColor = true;
+            this.SpielHinzufügenButton.UseWaitCursor = true;
             // 
-            // textBox1
+            // titel
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(210, 23);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.titel.Location = new System.Drawing.Point(12, 12);
+            this.titel.Name = "titel";
+            this.titel.PlaceholderText = "Titel";
+            this.titel.Size = new System.Drawing.Size(210, 23);
+            this.titel.TabIndex = 1;
+            this.titel.UseWaitCursor = true;
+            this.titel.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // Installationsdatum
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 52);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(210, 23);
-            this.textBox2.TabIndex = 2;
+            this.Installationsdatum.Location = new System.Drawing.Point(12, 52);
+            this.Installationsdatum.Name = "Installationsdatum";
+            this.Installationsdatum.PlaceholderText = "Installationsdatum";
+            this.Installationsdatum.Size = new System.Drawing.Size(210, 23);
+            this.Installationsdatum.TabIndex = 2;
+            this.Installationsdatum.UseWaitCursor = true;
             // 
-            // textBox3
+            // zuletztGespielt
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 94);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(210, 23);
-            this.textBox3.TabIndex = 3;
+            this.zuletztGespielt.Location = new System.Drawing.Point(12, 94);
+            this.zuletztGespielt.Name = "zuletztGespielt";
+            this.zuletztGespielt.PlaceholderText = "Zuletzt gespielt";
+            this.zuletztGespielt.Size = new System.Drawing.Size(210, 23);
+            this.zuletztGespielt.TabIndex = 3;
+            this.zuletztGespielt.UseWaitCursor = true;
             // 
-            // textBox4
+            // installationspfad
             // 
-            this.textBox4.Location = new System.Drawing.Point(12, 137);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(210, 23);
-            this.textBox4.TabIndex = 4;
+            this.installationspfad.Location = new System.Drawing.Point(12, 137);
+            this.installationspfad.Name = "installationspfad";
+            this.installationspfad.PlaceholderText = "Installationspfad";
+            this.installationspfad.Size = new System.Drawing.Size(210, 23);
+            this.installationspfad.TabIndex = 4;
+            this.installationspfad.UseWaitCursor = true;
+            // 
+            // kategorie
+            // 
+            this.kategorie.Location = new System.Drawing.Point(12, 175);
+            this.kategorie.Name = "kategorie";
+            this.kategorie.PlaceholderText = "Kategorie";
+            this.kategorie.Size = new System.Drawing.Size(210, 23);
+            this.kategorie.TabIndex = 5;
+            this.kategorie.UseWaitCursor = true;
+            // 
+            // publisher
+            // 
+            this.publisher.Location = new System.Drawing.Point(12, 213);
+            this.publisher.Name = "publisher";
+            this.publisher.PlaceholderText = "Publisher";
+            this.publisher.Size = new System.Drawing.Size(210, 23);
+            this.publisher.TabIndex = 6;
+            this.publisher.UseWaitCursor = true;
+            // 
+            // usk
+            // 
+            this.usk.FormattingEnabled = true;
+            this.usk.Location = new System.Drawing.Point(12, 254);
+            this.usk.Name = "usk";
+            this.usk.Size = new System.Drawing.Size(210, 23);
+            this.usk.TabIndex = 7;
             // 
             // NeuesSpielHinzufügen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(245, 299);
+            this.ClientSize = new System.Drawing.Size(245, 416);
             this.ControlBox = false;
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.usk);
+            this.Controls.Add(this.publisher);
+            this.Controls.Add(this.kategorie);
+            this.Controls.Add(this.installationspfad);
+            this.Controls.Add(this.zuletztGespielt);
+            this.Controls.Add(this.Installationsdatum);
+            this.Controls.Add(this.titel);
             this.Controls.Add(this.SpielHinzufügenButton);
             this.Name = "NeuesSpielHinzufügen";
             this.UseWaitCursor = true;
@@ -96,9 +137,12 @@
         #endregion
 
         private Button SpielHinzufügenButton;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
+        private TextBox titel;
+        private TextBox Installationsdatum;
+        private TextBox zuletztGespielt;
+        private TextBox installationspfad;
+        private TextBox kategorie;
+        private TextBox publisher;
+        private ComboBox usk;
     }
 }
