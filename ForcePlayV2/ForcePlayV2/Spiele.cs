eed9a0c3059d33be0876ab78c4c2cd557ab6dd0d
Raphael Tack <raphael.t7@gso.schule.koeln>
2023-03-10 17:41:04 +0100

    Fertigstellung der Spiele-Klasse:
    Folgende Änderungen wurden vorgenommen:
    	- Allen Attribute wurde im Konstruktor einen Wert zugewiesen
    	- Drei Methoden wurden erstellt.
    		- SpieleHinzufügen()
    		- SpieleLöschen()
    		- SpieleStarten()

--- ForcePlayV2/ForcePlayV2/Spiele.cs
+++ ForcePlayV2/ForcePlayV2/Spiele.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace ForcePlayV2
 {
-    public class Spiele
+     class Spiele
     {
+
+        // Die Attribute der Spiele
         string titel;
         string installationsdatum;
         string zuletzt;
@@ -16,6 +18,21 @@ namespace ForcePlayV2
         string publisher;
         string usk;
 
+        List<string> spieleVerwalten = new List<String>();
+
+        // Um Fehleingaben micht prüfen zu müssen, werden allen Attributen direkt ein Wert zugewiesen.
+        public Spiele()
+        {
+            titel = "";
+            installationsdatum = "";
+            zuletzt = "";
+            installationspfad = "";
+            kategorie = "";
+            publisher = "";
+            usk = "";
+        }
+
+        // Getter und Setter der jeweiligen Attribute.
         public string Titel { get => titel; set => titel = value; }
         public string Installationsdatum { get => installationsdatum; set => installationsdatum = value; }
         public string Zuletzt { get => zuletzt; set => zuletzt = value; }
@@ -23,5 +40,29 @@ namespace ForcePlayV2
         public string Kategorie { get => kategorie; set => kategorie = value; }
         public string Publisher { get => publisher; set => publisher = value; }
         public string Usk { get => usk; set => usk = value; }
+
+        /// <summary>
+        /// Diese Methode soll Spiele der jeweiligen Liste hinzufügen
+        /// </summary>
+        public void SpieleHinzufügen()
+        {
+
+        }
+
+        /// <summary>
+        /// Diese Methode soll Spiele löschen
+        /// </summary>
+        public void SpieleLöschen()
+        {
+
+        }
+
+        /// <summary>
+        /// Diese Methode soll die Spiele starten können
+        /// </summary>
+        public void SpieleStarten()
+        {
+
+        }
     }
 }
