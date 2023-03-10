bdc26613244bc4cd97062d4c035aecda9558d43b
Raphael Tack <raphael.t7@gso.schule.koeln>
2023-03-10 17:23:38 +0100

    Festlegung der Geter und Setter:
    Folgende Änderungen wurden vorgenommen:
    	- Die Variabel des vorherigen Commits wurden ergänzt
    	- Die Getter und Setter wurden gesetzt

--- ForcePlayV2/ForcePlayV2/Spiele.cs
+++ ForcePlayV2/ForcePlayV2/Spiele.cs
@@ -8,6 +8,20 @@ namespace ForcePlayV2
 {
     public class Spiele
     {
+        string titel;
+        string installationsdatum;
+        string zuletzt;
+        string installationspfad;
+        string kategorie;
+        string publisher;
+        string usk;
 
+        public string Titel { get => titel; set => titel = value; }
+        public string Installationsdatum { get => installationsdatum; set => installationsdatum = value; }
+        public string Zuletzt { get => zuletzt; set => zuletzt = value; }
+        public string Installationspfad { get => installationspfad; set => installationspfad = value; }
+        public string Kategorie { get => kategorie; set => kategorie = value; }
+        public string Publisher { get => publisher; set => publisher = value; }
+        public string Usk { get => usk; set => usk = value; }
     }
 }
