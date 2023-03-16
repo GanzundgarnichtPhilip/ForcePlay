3645e77afdb50f04ba58ff48b908d428da673a37
Daniel Sauer <daniel.s93@gso.schule.koeln>
2023-03-16 21:51:53 +0100

    Implementierung der Fehlermeldung.
    Es wurde eine Fehlermeldung in Form eines Forms
    erstellt, die dann auftaucht, wenn dem Benutzer
    eine Fehleingabe bei dem Hinzufügen eines neuen
    Spiels in den ForcePlay Game Launcher passiert.

---
+++ ForcePlayV2/ForcePlayV2/Fehlermeldung.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ForcePlayV2
+{
+    partial class Fehlermeldung
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ueberschrift_label = new Label();
+            ok_button = new Button();
+            fehlerbeschreibung_textBox = new TextBox();
+            SuspendLayout();
+            // 
+            // ueberschrift_label
+            // 
+            ueberschrift_label.BackColor = Color.Maroon;
+            ueberschrift_label.Font = new Font("Agency FB", 28.2F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            ueberschrift_label.ForeColor = Color.White;
+            ueberschrift_label.Location = new Point(0, 0);
+            ueberschrift_label.MaximumSize = new Size(530, 85);
+            ueberschrift_label.MinimumSize = new Size(530, 85);
+            ueberschrift_label.Name = "ueberschrift_label";
+            ueberschrift_label.Size = new Size(530, 85);
+            ueberschrift_label.TabIndex = 0;
+            ueberschrift_label.Text = "ACHTUNG";
+            ueberschrift_label.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // ok_button
+            // 
+            ok_button.Cursor = Cursors.Hand;
+            ok_button.Font = new Font("Agency FB", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            ok_button.ForeColor = Color.Black;
+            ok_button.Location = new Point(342, 247);
+            ok_button.MaximumSize = new Size(170, 50);
+            ok_button.MinimumSize = new Size(170, 50);
+            ok_button.Name = "ok_button";
+            ok_button.Size = new Size(170, 50);
+            ok_button.TabIndex = 1;
+            ok_button.Text = "OK";
+            ok_button.UseVisualStyleBackColor = true;
+            ok_button.Click += Ok_button_Click;
+            // 
+            // fehlerbeschreibung_textBox
+            // 
+            fehlerbeschreibung_textBox.BackColor = Color.White;
+            fehlerbeschreibung_textBox.BorderStyle = BorderStyle.None;
+            fehlerbeschreibung_textBox.Font = new Font("Agency FB", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            fehlerbeschreibung_textBox.ForeColor = Color.Black;
+            fehlerbeschreibung_textBox.Location = new Point(0, 85);
+            fehlerbeschreibung_textBox.MaximumSize = new Size(530, 150);
+            fehlerbeschreibung_textBox.MinimumSize = new Size(530, 150);
+            fehlerbeschreibung_textBox.Multiline = true;
+            fehlerbeschreibung_textBox.Name = "fehlerbeschreibung_textBox";
+            fehlerbeschreibung_textBox.ReadOnly = true;
+            fehlerbeschreibung_textBox.Size = new Size(530, 150);
+            fehlerbeschreibung_textBox.TabIndex = 2;
+            fehlerbeschreibung_textBox.Text = "Bitte füllen Sie alle Felder aus, um das von Ihnen gewünschte Spiel dem ForcePlay Game Launcher hinzufügen zu können.\r\n\r\nKlicken Sie auf \"OK\", um den ForcePlay Game Launcher weiter bedienen zu können.";
+            // 
+            // Fehlermeldung
+            // 
+            AutoScaleDimensions = new SizeF(120F, 120F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            AutoSize = true;
+            BackColor = Color.Maroon;
+            ClientSize = new Size(524, 309);
+            ControlBox = false;
+            Controls.Add(ok_button);
+            Controls.Add(fehlerbeschreibung_textBox);
+            Controls.Add(ueberschrift_label);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximumSize = new Size(530, 315);
+            MinimumSize = new Size(530, 315);
+            Name = "Fehlermeldung";
+            StartPosition = FormStartPosition.CenterScreen;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label ueberschrift_label;
+        private Button ok_button;
+        private TextBox fehlerbeschreibung_textBox;
+    }
+}
