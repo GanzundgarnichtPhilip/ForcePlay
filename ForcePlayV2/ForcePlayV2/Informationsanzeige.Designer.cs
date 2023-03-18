5b0144ad5f604b4916ad49f25cd1568e29abcddd
Raphael Tack <raphael.t7@gso.schule.koeln>
2023-03-18 15:12:42 +0100

    Behebung einiger Bugs:
    Folgende Änderungen wurden vorgenommen:
    	- Die Label wurden vertauscht bei MeineSpiele
    	- Kommentare wurden hinzugefügt
    	- Buttons wurden neu angeordnet
    	- Beschreibugnen wurden neu geschrieben
    	- Klassendiagramm wurde aktualisiert

--- ForcePlayV2/ForcePlayV2/Informationsanzeige.Designer.cs
+++ ForcePlayV2/ForcePlayV2/Informationsanzeige.Designer.cs
@@ -39,11 +39,12 @@
             ok_button.Cursor = Cursors.Hand;
             ok_button.Font = new Font("Agency FB", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
             ok_button.ForeColor = Color.Black;
-            ok_button.Location = new Point(342, 368);
-            ok_button.MaximumSize = new Size(170, 50);
-            ok_button.MinimumSize = new Size(170, 50);
+            ok_button.Location = new Point(272, 304);
+            ok_button.Margin = new Padding(2, 2, 2, 2);
+            ok_button.MaximumSize = new Size(136, 40);
+            ok_button.MinimumSize = new Size(136, 40);
             ok_button.Name = "ok_button";
-            ok_button.Size = new Size(170, 50);
+            ok_button.Size = new Size(136, 40);
             ok_button.TabIndex = 4;
             ok_button.Text = "OK";
             ok_button.UseVisualStyleBackColor = true;
@@ -55,13 +56,14 @@
             informationBeschreibung_textBox.BorderStyle = BorderStyle.None;
             informationBeschreibung_textBox.Font = new Font("Agency FB", 15F, FontStyle.Regular, GraphicsUnit.Point);
             informationBeschreibung_textBox.ForeColor = Color.Black;
-            informationBeschreibung_textBox.Location = new Point(0, 85);
-            informationBeschreibung_textBox.MaximumSize = new Size(530, 280);
-            informationBeschreibung_textBox.MinimumSize = new Size(530, 180);
+            informationBeschreibung_textBox.Location = new Point(0, 68);
+            informationBeschreibung_textBox.Margin = new Padding(2, 2, 2, 2);
+            informationBeschreibung_textBox.MaximumSize = new Size(424, 224);
+            informationBeschreibung_textBox.MinimumSize = new Size(424, 144);
             informationBeschreibung_textBox.Multiline = true;
             informationBeschreibung_textBox.Name = "informationBeschreibung_textBox";
             informationBeschreibung_textBox.ReadOnly = true;
-            informationBeschreibung_textBox.Size = new Size(530, 265);
+            informationBeschreibung_textBox.Size = new Size(424, 224);
             informationBeschreibung_textBox.TabIndex = 5;
             informationBeschreibung_textBox.Text = resources.GetString("informationBeschreibung_textBox.Text");
             // 
@@ -71,25 +73,27 @@
             ueberschrift_label.Font = new Font("Agency FB", 28.2F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
             ueberschrift_label.ForeColor = Color.Black;
             ueberschrift_label.Location = new Point(0, 0);
-            ueberschrift_label.MaximumSize = new Size(530, 85);
-            ueberschrift_label.MinimumSize = new Size(530, 85);
+            ueberschrift_label.Margin = new Padding(2, 0, 2, 0);
+            ueberschrift_label.MaximumSize = new Size(424, 68);
+            ueberschrift_label.MinimumSize = new Size(424, 68);
             ueberschrift_label.Name = "ueberschrift_label";
-            ueberschrift_label.Size = new Size(530, 85);
+            ueberschrift_label.Size = new Size(424, 68);
             ueberschrift_label.TabIndex = 3;
             ueberschrift_label.Text = "INFORMATION";
             ueberschrift_label.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Informationsanzeige
             // 
-            AutoScaleDimensions = new SizeF(120F, 120F);
+            AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
             BackColor = Color.Silver;
-            ClientSize = new Size(524, 430);
+            ClientSize = new Size(419, 355);
             ControlBox = false;
             Controls.Add(ok_button);
             Controls.Add(informationBeschreibung_textBox);
             Controls.Add(ueberschrift_label);
             FormBorderStyle = FormBorderStyle.Fixed3D;
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Informationsanzeige";
             StartPosition = FormStartPosition.CenterScreen;
             ResumeLayout(false);
--- ForcePlayV2/ForcePlayV2/MeineSpiele.Designer.cs
+++ ForcePlayV2/ForcePlayV2/MeineSpiele.Designer.cs
@@ -55,10 +55,10 @@
             publisher_label.AutoSize = true;
             publisher_label.Font = new Font("Agency FB", 15F, FontStyle.Italic, GraphicsUnit.Point);
             publisher_label.ImeMode = ImeMode.NoControl;
-            publisher_label.Location = new Point(359, 89);
+            publisher_label.Location = new Point(287, 81);
             publisher_label.Margin = new Padding(2, 0, 2, 0);
             publisher_label.Name = "publisher_label";
-            publisher_label.Size = new Size(84, 29);
+            publisher_label.Size = new Size(65, 24);
             publisher_label.TabIndex = 12;
             publisher_label.Text = "Publisher";
             // 
@@ -68,11 +68,11 @@
             spieleListBox.Font = new Font("Agency FB", 15F, FontStyle.Regular, GraphicsUnit.Point);
             spieleListBox.ForeColor = Color.Black;
             spieleListBox.FormattingEnabled = true;
-            spieleListBox.ItemHeight = 29;
-            spieleListBox.Location = new Point(20, 20);
+            spieleListBox.ItemHeight = 24;
+            spieleListBox.Location = new Point(16, 16);
             spieleListBox.Margin = new Padding(2);
             spieleListBox.Name = "spieleListBox";
-            spieleListBox.Size = new Size(280, 700);
+            spieleListBox.Size = new Size(225, 556);
             spieleListBox.Sorted = true;
             spieleListBox.TabIndex = 9;
             spieleListBox.SelectedIndexChanged += SpieleListBox_SelectedIndexChanged;
@@ -81,10 +81,10 @@
             // 
             titel_label.AutoSize = true;
             titel_label.Font = new Font("Agency FB", 19.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            titel_label.Location = new Point(359, 20);
+            titel_label.Location = new Point(287, 21);
             titel_label.Margin = new Padding(2, 0, 2, 0);
             titel_label.Name = "titel_label";
-            titel_label.Size = new Size(132, 41);
+            titel_label.Size = new Size(103, 34);
             titel_label.TabIndex = 13;
             titel_label.Text = "Game Titel";
             // 
@@ -92,10 +92,10 @@
             // 
             genres_label.AutoSize = true;
             genres_label.Font = new Font("Agency FB", 15F, FontStyle.Italic, GraphicsUnit.Point);
-            genres_label.Location = new Point(359, 152);
+            genres_label.Location = new Point(287, 132);
             genres_label.Margin = new Padding(2, 0, 2, 0);
             genres_label.Name = "genres_label";
-            genres_label.Size = new Size(58, 29);
+            genres_label.Size = new Size(45, 24);
             genres_label.TabIndex = 14;
             genres_label.Text = "Genre";
             // 
@@ -103,10 +103,10 @@
             // 
             zuletztGesp_label.AutoSize = true;
             zuletztGesp_label.Font = new Font("Agency FB", 15F, FontStyle.Italic, GraphicsUnit.Point);
-            zuletztGesp_label.Location = new Point(359, 214);
+            zuletztGesp_label.Location = new Point(287, 229);
             zuletztGesp_label.Margin = new Padding(2, 0, 2, 0);
             zuletztGesp_label.Name = "zuletztGesp_label";
-            zuletztGesp_label.Size = new Size(123, 29);
+            zuletztGesp_label.Size = new Size(96, 24);
             zuletztGesp_label.TabIndex = 15;
             zuletztGesp_label.Text = "Zuletzt gespielt";
             // 
@@ -114,10 +114,10 @@
             // 
             instPfad_label.AutoSize = true;
             instPfad_label.Font = new Font("Agency FB", 15F, FontStyle.Italic, GraphicsUnit.Point);
-            instPfad_label.Location = new Point(359, 402);
+            instPfad_label.Location = new Point(287, 332);
             instPfad_label.Margin = new Padding(2, 0, 2, 0);
             instPfad_label.Name = "instPfad_label";
-            instPfad_label.Size = new Size(133, 29);
+            instPfad_label.Size = new Size(103, 24);
             instPfad_label.TabIndex = 16;
             instPfad_label.Text = "Installationspfad";
             // 
@@ -125,10 +125,10 @@
             // 
             instDatum_label.AutoSize = true;
             instDatum_label.Font = new Font("Agency FB", 15F, FontStyle.Italic, GraphicsUnit.Point);
-            instDatum_label.Location = new Point(359, 339);
+            instDatum_label.Location = new Point(287, 281);
             instDatum_label.Margin = new Padding(2, 0, 2, 0);
             instDatum_label.Name = "instDatum_label";
-            instDatum_label.Size = new Size(148, 29);
+            instDatum_label.Size = new Size(115, 24);
             instDatum_label.TabIndex = 17;
             instDatum_label.Text = "Installationsdatum";
             // 
@@ -136,56 +136,56 @@
             // 
             usk_label.AutoSize = true;
             usk_label.Font = new Font("Agency FB", 15F, FontStyle.Italic, GraphicsUnit.Point);
-            usk_label.Location = new Point(359, 277);
+            usk_label.Location = new Point(287, 178);
             usk_label.Margin = new Padding(2, 0, 2, 0);
             usk_label.Name = "usk_label";
-            usk_label.Size = new Size(124, 29);
+            usk_label.Size = new Size(98, 24);
             usk_label.TabIndex = 19;
             usk_label.Text = "USK-Einstufung";
             // 
             // publisher
             // 
             publisher.Font = new Font("Agency FB", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            publisher.Location = new Point(532, 93);
+            publisher.Location = new Point(426, 74);
             publisher.Margin = new Padding(2);
             publisher.Name = "publisher";
-            publisher.Size = new Size(870, 37);
+            publisher.Size = new Size(697, 31);
             publisher.TabIndex = 20;
             // 
             // genres
             // 
             genres.Font = new Font("Agency FB", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            genres.Location = new Point(532, 156);
+            genres.Location = new Point(426, 125);
             genres.Margin = new Padding(2);
             genres.Name = "genres";
-            genres.Size = new Size(870, 37);
+            genres.Size = new Size(697, 31);
             genres.TabIndex = 21;
             // 
             // usk
             // 
             usk.Font = new Font("Agency FB", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            usk.Location = new Point(532, 214);
+            usk.Location = new Point(426, 171);
             usk.Margin = new Padding(2);
             usk.Name = "usk";
-            usk.Size = new Size(870, 37);
+            usk.Size = new Size(697, 31);
             usk.TabIndex = 23;
             // 
             // instPfad
             // 
             instPfad.Font = new Font("Agency FB", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            instPfad.Location = new Point(532, 406);
+            instPfad.Location = new Point(426, 325);
             instPfad.Margin = new Padding(2);
             instPfad.Name = "instPfad";
-            instPfad.Size = new Size(830, 37);
+            instPfad.Size = new Size(665, 31);
             instPfad.TabIndex = 25;
             // 
             // titel
             // 
             titel.Font = new Font("Agency FB", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            titel.Location = new Point(532, 20);
+            titel.Location = new Point(426, 16);
             titel.Margin = new Padding(2);
             titel.Name = "titel";
-            titel.Size = new Size(870, 47);
+            titel.Size = new Size(697, 39);
             titel.TabIndex = 26;
             // 
             // del_button
@@ -193,10 +193,10 @@
             del_button.BackColor = Color.FromArgb(255, 192, 255);
             del_button.Cursor = Cursors.Hand;
             del_button.Font = new Font("Agency FB", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            del_button.Location = new Point(1132, 468);
+            del_button.Location = new Point(906, 374);
             del_button.Margin = new Padding(2);
             del_button.Name = "del_button";
-            del_button.Size = new Size(270, 100);
+            del_button.Size = new Size(216, 80);
             del_button.TabIndex = 27;
             del_button.Text = "Löschen";
             del_button.UseVisualStyleBackColor = false;
@@ -207,10 +207,10 @@
             sfChanges_button.BackColor = Color.FromArgb(192, 255, 192);
             sfChanges_button.Cursor = Cursors.Hand;
             sfChanges_button.Font = new Font("Agency FB", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            sfChanges_button.Location = new Point(833, 468);
+            sfChanges_button.Location = new Point(666, 374);
             sfChanges_button.Margin = new Padding(2);
             sfChanges_button.Name = "sfChanges_button";
-            sfChanges_button.Size = new Size(270, 100);
+            sfChanges_button.Size = new Size(216, 80);
             sfChanges_button.TabIndex = 28;
             sfChanges_button.Text = "Änderungen speichern";
             sfChanges_button.UseVisualStyleBackColor = false;
@@ -221,10 +221,10 @@
             startGame_button.BackColor = Color.FromArgb(255, 255, 192);
             startGame_button.Cursor = Cursors.Hand;
             startGame_button.Font = new Font("Agency FB", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            startGame_button.Location = new Point(532, 468);
+            startGame_button.Location = new Point(426, 374);
             startGame_button.Margin = new Padding(2);
             startGame_button.Name = "startGame_button";
-            startGame_button.Size = new Size(270, 100);
+            startGame_button.Size = new Size(216, 80);
             startGame_button.TabIndex = 29;
             startGame_button.Text = "Starten";
             startGame_button.UseVisualStyleBackColor = false;
@@ -233,38 +233,40 @@
             // zuletztGesp
             // 
             zuletztGesp.Font = new Font("Agency FB", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            zuletztGesp.Location = new Point(532, 277);
+            zuletztGesp.Location = new Point(426, 222);
             zuletztGesp.Margin = new Padding(2);
             zuletztGesp.Name = "zuletztGesp";
-            zuletztGesp.Size = new Size(870, 37);
+            zuletztGesp.Size = new Size(697, 31);
             zuletztGesp.TabIndex = 22;
             // 
             // instDatum
             // 
             instDatum.Font = new Font("Agency FB", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            instDatum.Location = new Point(532, 343);
+            instDatum.Location = new Point(426, 274);
+            instDatum.Margin = new Padding(2);
             instDatum.Name = "instDatum";
-            instDatum.Size = new Size(870, 37);
+            instDatum.Size = new Size(697, 31);
             instDatum.TabIndex = 31;
             // 
             // explorer_Button
             // 
             explorer_Button.Cursor = Cursors.Hand;
             explorer_Button.Image = (Image)resources.GetObject("explorer_Button.Image");
-            explorer_Button.Location = new Point(1364, 405);
+            explorer_Button.Location = new Point(1091, 324);
+            explorer_Button.Margin = new Padding(2);
             explorer_Button.Name = "explorer_Button";
-            explorer_Button.Size = new Size(38, 38);
+            explorer_Button.Size = new Size(30, 30);
             explorer_Button.TabIndex = 32;
             explorer_Button.UseVisualStyleBackColor = true;
             explorer_Button.Click += Explorer_Button_Click;
             // 
             // MeineSpiele
             // 
-            AutoScaleDimensions = new SizeF(120F, 120F);
+            AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
             AutoSize = true;
             BackColor = Color.Silver;
-            ClientSize = new Size(1432, 782);
+            ClientSize = new Size(1146, 626);
             ControlBox = false;
             Controls.Add(explorer_Button);
             Controls.Add(instDatum);
